cf840a325226549d3fea2f3634fcd1aeba05582b
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-07-15 15:24:35 +0200

    works schema

--- src/components/works/WorksEditor.tsx
+++ src/components/works/WorksEditor.tsx
@@ -1,12 +1,70 @@
 import Form from "@rjsf/mui";
 import validator from "@rjsf/validator-ajv8";
-import { RJSFSchema, UiSchema } from "@rjsf/utils";
+import { FieldProps, RJSFSchema, UiSchema, WidgetProps } from "@rjsf/utils";
 import formatJSONSchema from "../../utils/formatJSONSchema";
 import worksSchema from "./worksSchema.json";
+import CustomAutocomplete from "../AutocompleteMultipleFreesolo";
+import ImagesBlock from "../images/ImagesBlock";
+import fetchData from "../../utils/fetchData";
+import { Events } from "./worksSchema";
 
 export default function WorksEditor() {
   const schema: RJSFSchema = formatJSONSchema(worksSchema);
-  const uiSchema: UiSchema = {};
+  const uiSchema: UiSchema = {
+    general: {
+      tags: {
+        "ui:field": (props: FieldProps) => (
+          <CustomAutocomplete
+            value={props.formData}
+            onChange={props.onChange}
+            label="Tags"
+            freeSolo
+            fetchOptions={() => fetchData("tags")}
+          />
+        ),
+      },
+    },
+    details: {
+      events: {
+        // autocomplete must be modified
+        "ui:field": (props: FieldProps<Events>) => (
+          <CustomAutocomplete
+            value={props.formData}
+            onChange={props.onChange}
+            label="Events"
+            multiple={false}
+            fetchOptions={() => fetchData("events")}
+          />
+        ),
+      },
 
-  return <Form schema={schema} uiSchema={uiSchema} validator={validator} />;
+      medium: {
+        "ui:field": (props: FieldProps) => (
+          <CustomAutocomplete
+            value={props.formData}
+            onChange={props.onChange}
+            label="Medium"
+            freeSolo
+            fetchOptions={() => fetchData("works/medium-list")}
+          />
+        ),
+      },
+
+      images: {
+        "ui:widget": (props: WidgetProps) => (
+          <ImagesBlock onChange={props.onChange} value={props.value} />
+        ),
+      },
+    },
+  };
+  const onSubmit = (data) => console.log("Data submitted: ", data.formData);
+
+  return (
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      validator={validator}
+      onSubmit={onSubmit}
+    />
+  );
 }
