1a9713381335ee7614905c4f22f49272b0717567
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-12-28 15:58:41 +0100

    display createdAt field in Post update page

--- src/components/TextBlock.tsx
+++ src/components/TextBlock.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { Box, CircularProgress, Typography, useTheme } from '@mui/material';
 import useDarkMode from '../hooks/useDarkMode';
@@ -10,91 +10,94 @@ interface TextBlockProps {
   name?: string;
 }
 
-const TextBlock: React.FC<TextBlockProps> = ({
-  id,
-  value = '',
-  onBlur,
-  name = 'Text'
-}) => {
-  const [editorContent, setEditorContent] = useState<string>(value);
-  const [loading, setLoading] = useState(true);
+const TextBlock: React.FC<TextBlockProps> = React.memo(
+  ({ id, value = '', onBlur, name = 'Text' }) => {
+    const [editorContent, setEditorContent] = useState<string>(value);
+    const [loading, setLoading] = useState(true);
+    const theme = useTheme();
 
-  const handleEditorChange = (content: string) => {
-    setEditorContent(content);
-  };
+    const handleEditorChange = useCallback((content: string) => {
+      setEditorContent(content);
+    }, []);
 
-  const handleEditorBlur = () => {
-    if (typeof onBlur === 'function') {
-      onBlur(id, editorContent);
-    }
-  };
+    const handleEditorBlur = useCallback(() => {
+      if (onBlur) {
+        onBlur(id, editorContent);
+      }
+    }, [editorContent, id, onBlur]);
 
-  const theme = useTheme();
-
-  return (
-    <>
-      {loading && (
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '500px',
-            width: '100%',
-            position: 'absolute',
-            left: '50%',
-            top: '50%',
-            transform: 'translate(-50%, -50%)',
-            overflow: 'hidden'
+    return (
+      <>
+        {loading && (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '500px',
+              width: '100%',
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%, -50%)',
+              overflow: 'hidden'
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        )}
+        <Typography variant="caption" color="textSecondary">
+          {name}
+        </Typography>
+        <Editor
+          tinymceScriptSrc={`${import.meta.env.BASE_URL}/tinymce/tinymce.min.js`}
+          licenseKey="gpl"
+          id={id}
+          initialValue={value}
+          init={{
+            skin: useDarkMode() ? 'oxide-dark' : 'oxide',
+            content_style: useDarkMode()
+              ? `body {background-color: ${theme.palette.background.paper}; color: ${theme.palette.common.white};}`
+              : '',
+            menubar: false,
+            branding: false,
+            plugins: [
+              'advlist',
+              'autolink',
+              'lists',
+              'link',
+              'charmap',
+              'anchor',
+              'searchreplace',
+              'code',
+              'fullscreen',
+              'insertdatetime',
+              'table',
+              'help',
+              'wordcount',
+              'table'
+            ],
+            toolbar:
+              'undo redo | blocks | ' +
+              'bold italic forecolor | link | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | table | help | code',
+            height: 500
           }}
-        >
-          <CircularProgress />
-        </Box>
-      )}
-      <Typography variant="caption" color="textSecondary">
-        {name}
-      </Typography>
-      <Editor
-        tinymceScriptSrc={`${import.meta.env.BASE_URL}/tinymce/tinymce.min.js`}
-        licenseKey="gpl"
-        id={id}
-        initialValue={value}
-        init={{
-          skin: useDarkMode() ? 'oxide-dark' : 'oxide',
-          content_style: useDarkMode()
-            ? `body {background-color: ${theme.palette.background.paper}; color: ${theme.palette.common.white}; }} `
-            : '',
-          menubar: false,
-          branding: false,
-          plugins: [
-            'advlist',
-            'autolink',
-            'lists',
-            'link',
-            'charmap',
-            'anchor',
-            'searchreplace',
-            'code',
-            'fullscreen',
-            'insertdatetime',
-            'table',
-            'help',
-            'wordcount',
-            'table'
-          ],
-          toolbar:
-            'undo redo | blocks | ' +
-            'bold italic forecolor | link | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | table | help | code',
-          height: '500'
-        }}
-        onInit={() => setLoading(false)}
-        onEditorChange={handleEditorChange}
-        onBlur={handleEditorBlur}
-      />
-    </>
-  );
-};
+          onInit={() => setLoading(false)}
+          onEditorChange={handleEditorChange}
+          onBlur={handleEditorBlur}
+        />
+      </>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.id === nextProps.id &&
+      prevProps.value === nextProps.value &&
+      prevProps.name === nextProps.name
+    );
+  }
+);
 
 export default TextBlock;
